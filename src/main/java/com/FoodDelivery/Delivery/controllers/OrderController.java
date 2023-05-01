4360a88d289ea0e374e4a0f787aff7a725aa5577
lux <ssbekmuratov@gmail.com>
2023-05-01 23:05:40 +0600

    set up

--- src/main/java/com/FoodDelivery/Delivery/controllers/OrderController.java
+++ src/main/java/com/FoodDelivery/Delivery/controllers/OrderController.java
@@ -1,4 +1,16 @@
 package com.FoodDelivery.Delivery.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/delicious-foods")
 public class OrderController {
+
+    @GetMapping
+    public String mainPage(){
+        return "This is delivery";
+    }
+
 }
--- src/main/java/com/FoodDelivery/Delivery/models/FoodModel.java
+++ src/main/java/com/FoodDelivery/Delivery/models/FoodModel.java
@@ -1,4 +1,8 @@
 package com.FoodDelivery.Delivery.models;
 
 public class FoodModel {
+    private Long id;
+    private String name;
+    private String description;
+    private Integer price;
 }
