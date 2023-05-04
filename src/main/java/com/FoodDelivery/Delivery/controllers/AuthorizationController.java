fbaaf93da18628763116a796dc0abb3713e7b4b6
lux <ssbekmuratov@gmail.com>
2023-05-04 10:53:01 +0600

    swagger is added

--- src/main/java/com/FoodDelivery/Delivery/controllers/AuthorizationController.java
+++ src/main/java/com/FoodDelivery/Delivery/controllers/AuthorizationController.java
@@ -1,14 +1,18 @@
 package com.FoodDelivery.Delivery.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/authorization")
+@Api(description = "Authorization endpoints")
 public class AuthorizationController {
 
     @GetMapping
+    @ApiOperation("Authorization page")
     public String authorization(){
         return "Authorization page";
     }
--- src/main/java/com/FoodDelivery/Delivery/controllers/OrderController.java
+++ src/main/java/com/FoodDelivery/Delivery/controllers/OrderController.java
@@ -1,14 +1,18 @@
 package com.FoodDelivery.Delivery.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/delicious-foods")
+@Api(description = "Order endpoints")
 public class OrderController {
 
     @GetMapping
+    @ApiOperation("Main page of site")
     public String mainPage(){
         return "This is delivery";
     }
